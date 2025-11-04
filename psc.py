d96985408c436b7d10a2971726b1a99dd800f3dc
Deepika-SenthilKumar <deepisk1994@gmail.com>
2025-11-04 12:38:54 +0530

    Refactor Streamlit app for traffic stops dashboard

--- psc.py
+++ psc.py
@@ -17,7 +17,7 @@ def create_connection():
         st.error(f"Error connecting to database: {e}")
         return None 
     
-#fetch data from database
+#fetch data FROM database
 def fetch_data(query):
     connection = create_connection()
     if connection:
@@ -71,7 +71,7 @@ with col4:
 #--------------------------list of query------------------------------
 
 st.header("Traffic Stops queries")
-select_query=st.selectbox("Select Query to run ",[
+SELECT_query=st.SELECTbox("SELECT Query to run ",[
     "Top 10 vehicle numbers in drug-related stops",
     "Vehicles most frequently searched",
     "Driver age group with highest arrest rate",
@@ -104,16 +104,18 @@ query_map = {
              GROUP BY vehicle_number 
              ORDER BY count DESC LIMIT 10""",
     "Vehicles most frequently searched":
-            """SELECT vehicle_number, AVG(search_conducted=1) AS search_rate
+            """SELECT vehicle_number,count(*) as search_rate
              FROM traffic
+             WHERE search_conducted = 1
              GROUP BY vehicle_number 
              ORDER BY search_rate DESC LIMIT 10""",
 
     "Driver age group with highest arrest rate":
-            """SELECT driver_age, MAX(is_arrested='1') AS arrest_rate ,count(*) AS total_stops
-             FROM traffic
-             GROUP BY driver_age
-             ORDER BY total_stops DESC""",
+           """SELECT driver_age, count(*) as arrest_rate 
+            FROM traffic
+            GROUP BY driver_age 
+            ORDER BY arrest_rate desc limit 10""",
+
 
     "Gender distribution by country":
             """SELECT country_name, driver_gender, COUNT(*) AS count 
@@ -148,10 +150,10 @@ query_map = {
              ORDER BY arrest_rate DESC""",
 
     "Violations associated with searches/arrests":
-            """select violation, 
+            """SELECT violation, 
              count(*) as total_arrest 
-             from traffic
-             where stop_outcome like 'arrest' group by violation""",
+             FROM traffic
+             where stop_outcome like 'arrest' GROUP BY violation""",
 
     "Violations most common among drivers <25":
             """SELECT violation, COUNT(*) AS count 
@@ -162,11 +164,11 @@ query_map = {
             
 
     "Violations rarely resulting in search/arrest":
-            """select violation, 
+            """SELECT violation, 
              count(*) as count, 
              SUM(CASE WHEN search_conducted = True then 1 else 0 end) as total_search, 
              SUM(CASE WHEN is_arrested = TRUE THEN 1 ELSE 0 END) AS total_arrests 
-             from traffic group by violation""",
+             FROM traffic GROUP BY violation""",
 
     "Countries with highest drug-related stops":
             """SELECT country_name, COUNT(*) AS count 
@@ -190,63 +192,63 @@ query_map = {
 
     "Yearly breakdown of stops & arrests by country":
            
-            """select country_name, yearly_breakdown, 
+            """SELECT country_name, yearly_breakdown, 
             SUM(total_stops) OVER (PARTITION BY country_name) AS total_stops, 
             SUM(total_arrests) OVER (PARTITION BY country_name) AS total_arrests 
-            from (select country_name, count(*) as total_stops, 
-            extract(year from stop_date) as yearly_breakdown,
+            FROM (SELECT country_name, count(*) as total_stops, 
+            extract(year FROM stop_date) as yearly_breakdown,
             sum(case when is_arrested = True then 1 else 0 end) as total_arrests 
-            from traffic 
-            group by country_name, extract(year from stop_date)) as yearly_data 
-            order by country_name, yearly_breakdown""",
+            FROM traffic 
+            GROUP BY country_name, extract(year FROM stop_date)) as yearly_data 
+            ORDER BY country_name, yearly_breakdown""",
 
     "Driver violation trends by age & race":
            
-            """select distinct v.violation, ps.driver_age, ps.driver_race 
-            from traffic ps join (select driver_age, driver_race, count(*) as violation 
-            from traffic group by driver_age, driver_race) as v 
+            """SELECT distinct v.violation, ps.driver_age, ps.driver_race 
+            FROM traffic ps join (SELECT driver_age, driver_race, count(*) as violation 
+            FROM traffic GROUP BY driver_age, driver_race) as v 
             on ps.driver_age = v.driver_age and ps.driver_race = v.driver_race 
-            order by v.violation desc""",
+            ORDER BY v.violation desc""",
 
     "Time period analysis of stops (Year/Month/Hour)":
                 
-            """select year(stop_date) as stop_year, 
+            """SELECT year(stop_date) as stop_year, 
             month(stop_date) as stop_month, 
             hour(stop_time) as stop_hour, count(*) as Number_of_stops 
-            from traffic group by stop_year, stop_month, stop_hour""",
+            FROM traffic GROUP BY stop_year, stop_month, stop_hour""",
 
     "Violations with High Search and Arrest Rates":
-            """select 
+            """SELECT 
             violation, count(*) as total_stops, 
             sum(case when search_conducted = True then 1 else 0 end) as total_search, 
             sum(case when is_arrested = True then 1 else 0 end) as total_arrest, 
-            rank() over (order by sum(case when search_conducted = True then 1 else 0 end)* 1.0 / Count(*)) as search, 
-            rank() over (order by sum(case when is_arrested = True then 1 else 0 end)* 1.0/ count(*)) as arrest 
-            from traffic 
-            group by violation""",
+            rank() over (ORDER BY sum(case when search_conducted = True then 1 else 0 end)* 1.0 / Count(*)) as search, 
+            rank() over (ORDER BY sum(case when is_arrested = True then 1 else 0 end)* 1.0/ count(*)) as arrest 
+            FROM traffic 
+            GROUP BY violation""",
     
     "Driver Demographics": 
-            """select 
+            """SELECT 
              country_name, driver_age, driver_gender, driver_race, count(*) as total_drivers
-             from traffic
-             group by country_name, driver_age, driver_gender, driver_race
-             order by country_name, driver_age, driver_gender, driver_race""",
+             FROM traffic
+             GROUP BY country_name, driver_age, driver_gender, driver_race
+             ORDER BY country_name, driver_age, driver_gender, driver_race""",
 
     "Top 5 violation with highest arrest": 
-            """select violation, count(*) as total_stops, 
+            """SELECT violation, count(*) as total_stops, 
              sum(case when is_arrested = True then 1 else 0 end) as total_arrest, 
              round(sum(case when is_arrested = True then 1 else 0 end) *1.0/ count(*), 2) as arrest_rate 
-             from traffic
-             group by violation order by total_arrest desc limit 5"""
+             FROM traffic
+             GROUP BY violation ORDER BY total_arrest desc limit 5"""
 }
 
     
 if st.button("Run Query"):
-    result= fetch_data(query_map[select_query])
+    result= fetch_data(query_map[SELECT_query])
     if not result.empty:
        st.write(result)
     else:
-        st.warning("No data found for the selected query.")
+        st.warning("No data found for the SELECTed query.")
 
 #-----------------------------------------Input form--------------------------------------
 st.header("add police log to predict outcome and violation")
@@ -255,17 +257,17 @@ with st.form("police_log_form"):
     stop_date= st.date_input("Stop Date")
     stop_time= st.time_input("Stop Time")   
     country_name= st.text_input("Country Name")
-    driver_gender= st.selectbox("Driver Gender",["M","F"])
+    driver_gender= st.SELECTbox("Driver Gender",["M","F"])
     driver_age= st.number_input("Driver Age",min_value=16,max_value=100,value=27)
-    driver_race= st.selectbox("Driver Race",["White","Black","Hispanic","Asian","Other"])
-    violation_raw= st.selectbox("Driver Violation",["Speeding","Drunk Driving","Seatbelt","Signal Violation","Other"])
-    search_conducted= st.selectbox("Search Conducted",[0,1])
-    search_type= st.selectbox("Search Type",["None","Vehicle Search","Frisk"])
-    stop_outcome= st.selectbox("Stop Outcome",["Ticket","Warning","Arrest"])
-    drugs_related_stop= st.selectbox("Drugs Related Stop",[0,1])   
-    is_arrested= st.selectbox("Is Arrested",[0,1])
+    driver_race= st.SELECTbox("Driver Race",["White","Black","Hispanic","Asian","Other"])
+    violation_raw= st.SELECTbox("Driver Violation",["Speeding","Drunk Driving","Seatbelt","Signal Violation","Other"])
+    search_conducted= st.SELECTbox("Search Conducted",[0,1])
+    search_type= st.SELECTbox("Search Type",["None","Vehicle Search","Frisk"])
+    stop_outcome= st.SELECTbox("Stop Outcome",["Ticket","Warning","Arrest"])
+    drugs_related_stop= st.SELECTbox("Drugs Related Stop",[0,1])   
+    is_arrested= st.SELECTbox("Is Arrested",[0,1])
     stop_duration= st.number_input("Stop Duration (in minutes)",min_value=1,max_value=300,value=15) 
-    violation_type= st.selectbox("Violation Type",["Speeding","DUI","Seatbelt","Equipment","Other"])
+    violation_type= st.SELECTbox("Violation Type",["Speeding","DUI","Seatbelt","Equipment","Other"])
     vehicle_number= st.text_input("Vehicle Number")
 
     timestamp= pd.Timestamp.now()
@@ -307,6 +309,7 @@ with st.form("police_log_form"):
 
                     Vehicle Number: **{vehicle_number}**.
                     """)
+
 
 
 
